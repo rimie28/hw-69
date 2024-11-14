fb564f1a2d09d90c218e2c0ac96ec5cedcb1411e
Meerim Maratova <maratovameerim@gmail.com>
2024-11-14 16:45:29 +0600

    made a component to show TV show info and finished App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import SearchTool from './containers/Search/SearchTool.tsx';
+import ShowInfo from './containers/ShowInfo/ShowInfo.tsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <Router>
+      <header className="bg-body-secondary p-3 mb-4">
+        <h2 className="text-dark">TV Shows</h2>
+      </header>
+      <SearchTool/>
+      <Routes>
+        <Route element={<ShowInfo/>} path="/shows/:id"></Route>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/containers/ShowInfo/ShowInfo.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { Show } from '../../types';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+
+const ShowInfo = () => {
+  const { id } = useParams<{ id: string }>();
+  const [show, setShow] = useState<Show>();
+
+  useEffect(() => {
+    const getShowInfo = async () => {
+      const response = await axios.get(`http://api.tvmaze.com/shows/${id}`);
+      setShow(response.data);
+    }
+    getShowInfo()
+  }, [id])
+
+  return (
+    <div className="container d-flex gap-3 mx-0 mt-4">
+        <div>
+          <img src={show?.image?.medium} alt={show?.name} className="rounded rounded-2" />
+        </div>
+        <div>
+          <h2 className="text-danger"> {show?.name}</h2>
+          <h5>Rating: {show?.rating.average}</h5>
+          <span><b>Genres: {show?.genres.join(', ')}</b></span>
+          <span dangerouslySetInnerHTML={{ __html: show?.summary || '' }}></span>
+        </div>
+    </div>
+  )
+}
+
+export default ShowInfo;
